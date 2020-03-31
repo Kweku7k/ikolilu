f6847a6a2af1fb3fd8bac8008063a6cb665424f3
Kweku7k <nkba@live.com>
2020-03-31 13:18:35 +0000

    tab is not responsive

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
-function standard(){
-    card = document.getElementsByClassName('standard')[0]
-    console.log(card)
-    includes = document.createElement('div')
-    includesHTML = `
-    <h6>HR Management</h6>
-    <h6>Payroll Management</h6>`
-    includes.innerHTML = includesHTML
-    card.append(includes) 
-} 
+// function show(event){
+//         target = element
+//         var element = document.getElementById("standard-hide");
+//         element.classList.toggle("show");
+// }
 
-function enterprise(){
-    card = document.getElementsByClassName('enterprise')[0]
-    console.log(card)
-    includes = document.createElement('div')
-    includesHTML = `
-    <h6>HR Management</h6>
-    <h6>Payroll Management</h6>
-    <h6>Mobile App</h6>
-    <h6>Transport Management</h6>
-    <h6>Canteen Management</h6>
-    <h6>Account Management</h6>`
-    includes.innerHTML = includesHTML
-    card.append(includes) 
+function enterprise() {
+    var x = document.getElementById("enterprise-hide");
+    console.log(x)
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
-    button = document.getElementsByClassName
-} 
+function show() {
+    var x = document.getElementById("standard-hide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
-function ultimate(){
-    card = document.getElementsByClassName('ultimate')[0]
-    console.log(card)
-    includes = document.createElement('div')
-    includesHTML = `
-    <h6>HR Management</h6>
-    <h6>Payroll Management</h6>
-    <h6>Mobile App</h6>
-    <h6>Transport Management</h6>
-    <h6>Canteen Management</h6>
-    <h6>Account Management</h6>
-    <h6>Library Management</h6>
-    <h6>Inventory Management</h6>`
-    includes.innerHTML = includesHTML
-    card.append(includes) 
-} 
+  function ultimate() {
+    var x = document.getElementById("ultimate-hide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+// function standard(){
+//     card = document.getElementsByClassName('standard')[0]
+//     console.log(card)
+//     includes = document.createElement('div')
+//     includesHTML = `
+//     <h6>HR Management</h6>
+//     <h6>Payroll Management</h6>`
+//     includes.innerHTML = includesHTML
+//     card.append(includes) 
+// } 
+
+
+
+// function enterprise(){
+//     card = document.getElementsByClassName('enterprise')[0]
+//     console.log(card)
+//     includes = document.createElement('div')
+//     includesHTML = `
+//     <h6>HR Management</h6>
+//     <h6>Payroll Management</h6>
+//     <h6>Mobile App</h6>
+//     <h6>Transport Management</h6>
+//     <h6>Canteen Management</h6>
+//     <h6>Account Management</h6>`
+//     includes.innerHTML = includesHTML
+//     card.append(includes) 
+
+//     button = document.getElementsByClassName
+// } 
+
+// function ultimate(){
+//     card = document.getElementsByClassName('ultimate')[0]
+//     console.log(card)
+//     includes = document.createElement('div')
+//     includesHTML = `
+//     <h6>HR Management</h6>
+//     <h6>Payroll Management</h6>
+//     <h6>Mobile App</h6>
+//     <h6>Transport Management</h6>
+//     <h6>Canteen Management</h6>
+//     <h6>Account Management</h6>
+//     <h6>Library Management</h6>
+//     <h6>Inventory Management</h6>`
+//     includes.innerHTML = includesHTML
+//     card.append(includes) 
+// } 
